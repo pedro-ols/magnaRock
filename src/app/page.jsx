f78215ead33974ba0d0386591b12de775e3d618b
Pedro de Oliveira Santos <pedrosantos.tds2.senai@gmail.com>
2025-07-05 17:20:27 -0300

    fix: adjust css structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <header className={styles.header}>
         <div className={styles.headerLogo}>
-          <img src="#" alt="Logo" className={styles.logoImage} />
+          <img src="#" alt="Logo" className={styles.headerLogoImage} />
         </div>
         <nav className={styles.navContainer}>
           <a href="/us" className={styles.navItem}><h1 className={styles.navText}>Nós</h1></a>
@@ -14,7 +14,11 @@ export default function Home() {
         </nav>
       </header>
       <div className={styles.homeHeroSection}>
-        <div className={styl}></div>
+        <div className={styles.introducingBanner}>
+          <img src="#" alt="Logo" className={styles.bannerLogoImage}/>
+          <h1 style={{ color: "var(--text-color)", opacity: "80%" }}>Conheça a </h1>
+          <h1 style={{ color: "var(--text-color)" }}>Magnarock</h1>
+        </div>
       </div>
     </div>
   );
